7455d4b51aee6dc7a579153835b0358c6ff8bdc8
deku-99 <75273987+deku-99@users.noreply.github.com>
2023-11-04 16:02:56 +0100

    uibuild

--- models/phonebook.js
+++ models/phonebook.js
@@ -10,41 +10,41 @@ console.log('connecting to', url)
 
 mongoose.connect(url)
 
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const phonebookSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        required: true,
-        validate: {
-            validator: function (value) {
-                // Regular expression to match the phone number format
-                const phoneRegex = /^\d+-\d+$/;
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        // Regular expression to match the phone number format
+        const phoneRegex = /^\d+-\d+$/
 
-                return phoneRegex.test(value);
-            },
-            message: 'Invalid phone number format. Please use the format: XXX-XXXXXXXX'
-        }
+        return phoneRegex.test(value)
+      },
+      message: 'Invalid phone number format. Please use the format: XXX-XXXXXXXX'
     }
+  }
 })
 
 
 phonebookSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 
--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -15,30 +15,30 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const phonebookSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const PhonebookEntry = mongoose.model('Phonebook', phonebookSchema)
 
 if (process.argv.length === 5) {
 
-    const entry = new PhonebookEntry({
-        name: process.argv[3],
-        number: process.argv[4]
-    })
+  const entry = new PhonebookEntry({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
 
-    entry.save().then(result => {
-        console.log(`added ${entry.name} number ${entry.number} to phonebook`)
-        mongoose.connection.close()
-    })
+  entry.save().then(() => {
+    console.log(`added ${entry.name} number ${entry.number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 else {
-    PhonebookEntry.find({}).then(result => {
-        console.log("phonebook:");
-        result.forEach(entry => {
-            console.log(`${entry.name} ${entry.number}`)
-        })
-        mongoose.connection.close()
+  PhonebookEntry.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(entry => {
+      console.log(`${entry.name} ${entry.number}`)
     })
+    mongoose.connection.close()
+  })
 }
